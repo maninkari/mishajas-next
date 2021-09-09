d1a61c33103a010a0d3909bc1c1c2eff1a3de3b6
roberto.torres <ts.roberto@gmail.com>
2021-09-09 10:24:52 +0100

    medicine

--- pages/medicine/index.jsx
+++ pages/medicine/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const Medicine = () => (
+const Medicine = (props) => (
   <div>
     <h1>Medicine</h1>
+    <div>
+      {props.data.sections.map((p) => {
+        return panel(p)
+      })}
+    </div>
+    <div>{JSON.stringify(props.data)}</div>
   </div>
 )
+
+const panel = (p) => {
+  return (
+    <a>
+      <h3>{p.title}</h3>
+      <p>{p.description}</p>
+    </a>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`http://localhost:3000/api/medicine`)
+  const content = await res.json()
+  return { props: content }
+}
 
 export default Medicine
